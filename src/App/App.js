8d15552d1e1cca46c92c05b00c83c88517e9dd0e
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2025-12-13 21:50:14 +0100

    cleaning empty spaces

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-     <Form />
+      <Form />
       <footer className="footer">©2023 Bugs. All rights reserved ©</footer>
     </div>
   );
--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -4,13 +4,13 @@ export const Result = ({ result }) => (
     <p className="result">
         {result !== undefined && (
             <>
-            {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-            {" "}
-            
-            <strong>
-                {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+                {" "}
+
+                <strong>
+                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
                 </strong>
-                </>
+            </>
         )}
     </p>
 );
