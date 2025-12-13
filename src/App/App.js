7e487299c6af12f1d6f0444e1e6d55308db89679
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2025-12-13 20:40:45 +0100

    styles and normalize.css added

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import Form from "./Form"
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+     <Form />
+      <footer className="footer">©2023 Bugs. All rights reserved ©</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/currencies.js
@@ -0,0 +1,27 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.4500,
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.0700,
+    },
+    {
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: 5.1700,
+    },
+    {
+        short: "CHF",
+        name: "Frank Szwajcarski",
+        rate: 4.5500,
+    },
+    {
+        short: "NOK",
+        name: "Korona Norweska",
+        rate: 0.3600,
+    },
+]
