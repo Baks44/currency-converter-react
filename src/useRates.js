be544e123d5bcd7a3cc3225ac9beb17b6e0b7841
Tomasz Krzeminski <tomasz.krzeminski4@gmail.com>
2026-02-11 18:55:08 +0100

    cleanup

--- src/useRates.js
+++ src/useRates.js
@@ -1,34 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export function useRates() {
-    const [ratesData, setRatesData] = useState({
-        state: "loading",
-    });
+  const [ratesData, setRatesData] = useState({
+    state: 'loading'
+  });
 
-    useEffect(() => {
-        const getRates = async () => {
-            try {
-                const response = await fetch(
-                    `https://api.currencyapi.com/v3/latest?apikey=${process.env.REACT_APP_CURRENCY_API_KEY}&base_currency=PLN`
-                );
+  useEffect(() => {
+    const getRates = async () => {
+      try {
+        const response = await fetch(
+          `https://api.currencyapi.com/v3/latest?apikey=${process.env.REACT_APP_CURRENCY_API_KEY}&base_currency=PLN`
+        );
 
-                if (!response.ok) throw new Error(response.statusText);
+        if (!response.ok) throw new Error(response.statusText);
 
-                const data = await response.json();
+        const data = await response.json();
 
-                setRatesData({
-                    state: "success",
-                    rates: data.data,
-                });
-            } catch {
-                setRatesData({
-                    state: "error",
-                });
-            }
-        };
+        setRatesData({
+          await: new Promise((resolve) => setTimeout(resolve, 1000)),
+          state: 'success',
+          rates: data.data
+        });
+      } catch {
+        setRatesData({
+          state: 'error'
+        });
+      }
+    };
 
-        getRates();
-    }, []);
+    getRates();
+  }, []);
 
-    return ratesData;
+  return ratesData;
 }
